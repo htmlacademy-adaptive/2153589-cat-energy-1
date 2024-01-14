052de4c9d4df0e3671b77228a19c275c14428332
Yuriy Frolov <yfrolou@mail.ru>
2024-01-14 15:29:53 +0300

    Добавлен сценарий: без JS

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,12 @@
 const toggle=document.querySelector(".header__toggle");
 const nav=document.querySelector(".header__nav");
+const header=document.querySelector(".header");
+
+toggle.classList.remove("header__toggle--nojs");
+nav.classList.remove("header__nav--nojs");
 
 toggle.addEventListener("click",()=>{
+    header.classList.toggle("header--sticky");
     nav.classList.toggle("header__nav--active");
     toggle.classList.toggle("header__toggle--active");
 });
