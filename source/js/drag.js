f69212b97824e794c8ae2089f1cb7a939a5538e8
Yuriy Frolov <yfrolou@mail.ru>
2024-01-20 20:50:07 +0300

    Исправляет подключение favicon согласно уроку

--- source/js/drag.js
+++ source/js/drag.js
@@ -7,7 +7,6 @@ const initPosition = draggableButton.offsetLeft;
 const parentCoords = dragTarget.getBoundingClientRect();
 
 
-
 const dragX = (event) => {
     const el = event.currentTarget;
     const move = (e) => {
@@ -16,13 +15,16 @@ const dragX = (event) => {
             el.style.left = `${el.offsetLeft + e.movementX}px`;
             catBefore.style.width = parseInt(el.style.left) + "px";
 
-             if ((el.offsetLeft + e.movementX) > initPosition) {
-                 catBeforeImage.style.objectFit="contain";
+            if ((el.offsetLeft + e.movementX) > initPosition) {
+
+                if ((el.offsetLeft + e.movementX) > (parentCoords.width - parentCoords.width / 3)) {
+                    catBeforeImage.style.objectFit = "contain";
+                }
                 catBeforeImage.style.marginLeft = (parentCoords.width - parseInt(el.style.left)) / parentCoords.width * 100 + "%";
-              } else if ((el.offsetLeft + e.movementX) <= initPosition) {
-                 catBeforeImage.style.objectFit="fill";
+            } else if ((el.offsetLeft + e.movementX) <= initPosition) {
+                catBeforeImage.style.objectFit = "fill";
                 catBefore.style.width = (parentCoords.width - (parentCoords.width - parseInt(el.style.left))) + "px";
-              }
+            }
         }
     };
     const up = () => {
