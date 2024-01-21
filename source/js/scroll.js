60330fd62954f4bb6cfa01e32d8e6bac8b62fce2
Yuriy Frolov <yfrolou@mail.ru>
2024-01-21 11:54:47 +0300

    Исправляет promo:before для корректного переполнения списка навигации

--- source/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const element = document.querySelector('.header__nav');
-    if (window.scrollY >= 50) {
-        element.classList.add('shadow');
-    } else {
-        element.classList.remove('shadow');
-    }
-});
