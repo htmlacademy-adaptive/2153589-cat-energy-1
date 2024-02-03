a39015e9b40b8d45f60531a9ea37fa3c8c6c6a96
Keks <keks@htmlacademy.ru>
2024-02-03 10:42:57 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/map.js
@@ -0,0 +1,26 @@
+function initMap() {
+    const mapBlock=document.querySelector(".contacts__map");
+    mapBlock.classList.remove("contacts__map--nojs");
+    const myLatLng = { lat: 59.96834114873084, lng: 30.317505457201793 };
+    const  tooltipTitle = 'HTML Academy\n' + 'наб. Pеки Карповки, 5П \n' + 'Санкт-Петербург\n' + 'Россия \n' + '197022';
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: myLatLng,
+        disableDefaultUI: false
+    });
+    const image = '../img/map-pin.png';
+    const marker=new google.maps.Marker({
+        position: myLatLng,
+        map,
+        icon: image,
+        title: tooltipTitle,
+        store_id: 1
+    });
+    const info = new google.maps.InfoWindow({
+        content: '<div><h4>HTML Academy</h4><p>наб. Pеки Карповки, 5П<br> Санкт-Петербург<br> Россия<br> 197022</p><p><a href="https://htmlacademy.ru/contacts">Свяжитесь с нами</a></p></div>'
+    });
+
+    marker.addListener("click", function() {
+        info.open(map, this);
+})
+}
