692480614b0edfe98a497dbd867cb940e923eefe
Yuriy Frolov <yfrolou@mail.ru>
2024-07-07 20:22:38 +0300

    Изменяет imgmin на squoosh в gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
@@ -7,25 +8,25 @@ import browser from 'browser-sync';
 import csso from 'postcss-csso';
 import terser from 'gulp-terser';
 import squoosh from 'gulp-squoosh';
-import { deleteAsync } from 'del';
+import {deleteAsync} from 'del';
 import htmlmin from 'gulp-htmlmin';
-import debug from 'gulp-debug';
-import imagemin, { mozjpeg, optipng, svgo } from 'gulp-imagemin';
-import { stacksvg } from "gulp-stacksvg";
-
+import svgstore from "gulp-svgstore";
+import svgo from "gulp-svgmin";
+import rename from "gulp-rename";
+import {stacksvg} from "gulp-stacksvg";
 
 
 // Styles
 
 export const styles = () => {
-    return gulp.src('source/less/style.less', { sourcemaps: true })
+    return gulp.src('source/less/style.less', {sourcemaps: true})
         .pipe(plumber())
         .pipe(less())
         .pipe(postcss([
             autoprefixer(),
             csso()
         ]))
-        .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+        .pipe(gulp.dest('build/css', {sourcemaps: '.'}))
         .pipe(browser.stream());
 };
 
@@ -33,7 +34,7 @@ export const styles = () => {
 
 export const html = () => {
     return gulp.src('source/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('build'))
         .pipe(browser.stream());
 };
@@ -49,27 +50,27 @@ export const scripts = () => {
 
 // Images
 
+export const copyPngJpg = () => {
+    return gulp.src(['source/img/**/*.{png,jpg}'])
+        .pipe(gulp.dest('build/img'))
+};
 export const optimizePngJpg = () => {
-    return gulp.src(['source/img/**/*.{png,jpg}', '!source/img/favicons/*.{png,jpg}'])
-        .pipe(imagemin([
-            mozjpeg({ quality: 75, progressive: true }),
-            optipng({ optimizationLevel: 5 })
-        ]))
-        .pipe(debug({title: 'optimizePngJpg'}))
+    return gulp.src(['source/img/**/*.{png,jpg}'])
+        .pipe(squoosh(( {filePath} ) => {
+            const imageExtension = path.extname(filePath);
+            const optionsForPng = { oxipng: {} };
+            const optionsForJpg = { mozjpeg: {} };
+            const options = imageExtension === ".png" ? optionsForPng : optionsForJpg;
+            return {
+                encodeOptions: options,
+            };
+        }
+        ))
         .pipe(gulp.dest('build/img'))
 };
 
-export const optimizeFavicons = () => {
-    return gulp.src(['source/img/favicons/*.{png,jpg}'])
-        .pipe(imagemin([
-            mozjpeg({ quality: 75, progressive: true }),
-            optipng({ optimizationLevel: 5 })
-        ]))
-        .pipe(gulp.dest('build/img/favicons'))
-};
 
 // WebP
-
 export const createWebp = () => {
     return gulp.src(['source/img/*.{png,jpg}', '!source/img/favicons/*.png'])
         .pipe(squoosh({
@@ -84,7 +85,7 @@ export const createWebp = () => {
 
 export const optimizeSvgFavicons = () => {
     return gulp.src(['source/img/favicons/*.svg'])
-        .pipe(imagemin([
+        .pipe(
             svgo({
                 plugins: [
                     {
@@ -96,15 +97,12 @@ export const optimizeSvgFavicons = () => {
                         active: false
                     }
                 ]
-            })
-        ]))
-        .pipe(debug({title: 'imagemin-optimize-favicons-svg'}))
+            }))
         .pipe(gulp.dest('build/img/favicons'))
 };
-
 export const optimizeSvgLogo = () => {
     return gulp.src(['source/img/logo/*.svg'])
-        .pipe(imagemin([
+        .pipe(
             svgo({
                 plugins: [
                     {
@@ -116,14 +114,13 @@ export const optimizeSvgLogo = () => {
                         active: false
                     }
                 ]
-            })
-        ]))
+            }))
         .pipe(gulp.dest('build/img/logo'))
 };
 
-export const stack = () => {
+export const spriteSvg = () => {
     return gulp.src(['source/img/*.svg', '!source/img/logo/*.svg', '!source/img/favicons/*.svg'])
-        .pipe(imagemin([
+        .pipe(
             svgo({
                 plugins: [
                     {
@@ -136,10 +133,11 @@ export const stack = () => {
                     }
                 ]
             })
-        ]))
+        )
         .pipe(stacksvg({ output: 'stack', separator: '__', spacer: '-' }))
         .pipe(gulp.dest('build/img'))
 };
+
 
 // Copy
 
@@ -187,7 +185,7 @@ const reload = (done) => {
 
 const watcher = () => {
     gulp.watch('source/less/**/*.less', gulp.series(styles));
-    gulp.watch('source/*.html',gulp.series(html));
+    gulp.watch('source/*.html', gulp.series(html));
     gulp.watch('source/js/*.js', gulp.series(scripts));
 };
 
@@ -197,15 +195,14 @@ const watcher = () => {
 export const build = gulp.series(
     clean,
     copy,
+    optimizePngJpg,
     gulp.parallel(
         styles,
         html,
         scripts,
-        optimizePngJpg,
-        optimizeFavicons,
-        optimizeSvgLogo,
         optimizeSvgFavicons,
-        stack,
+        optimizeSvgLogo,
+        spriteSvg,
         createWebp
     ),
 );
@@ -213,10 +210,17 @@ export const build = gulp.series(
 // Default
 
 export default gulp.series(
+    clean,
+    copy,
+    copyPngJpg,
     gulp.parallel(
         styles,
         html,
-        scripts
+        scripts,
+        optimizeSvgFavicons,
+        optimizeSvgLogo,
+        spriteSvg,
+        createWebp
     ),
     gulp.series(
         server,
