1d05743292956cf3fd556c056be38574afb74334
Yuriy Frolov <yfrolou@mail.ru>
2024-07-19 21:03:14 +0300

    Изменяет адрес академии на старый

--- source/js/map.js
+++ source/js/map.js
@@ -1,8 +1,8 @@
 function initMap() {
     const mapBlock=document.querySelector(".contacts__map");
     mapBlock.classList.remove("contacts__map--nojs");
-    const myLatLng = { lat: 59.96834114873084, lng: 30.317505457201793 };
-    const  tooltipTitle = 'HTML Academy\n' + 'наб. Pеки Карповки, 5П \n' + 'Санкт-Петербург\n' + 'Россия \n' + '197022';
+    const myLatLng = { lat: 59.93874872101732, lng: 30.32304739767776 };
+    const  tooltipTitle = 'HTML Academy\n' + 'ул. Большая Конюшенная, д. 19/8 \n' + 'Санкт-Петербург\n' + 'Россия \n' + '197022';
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: myLatLng,
@@ -17,7 +17,7 @@ function initMap() {
         store_id: 1
     });
     const info = new google.maps.InfoWindow({
-        content: '<div><h4>HTML Academy</h4><p>наб. Pеки Карповки, 5П<br> Санкт-Петербург<br> Россия<br> 197022</p><p><a href="https://htmlacademy.ru/contacts">Свяжитесь с нами</a></p></div>'
+        content: '<div><h4>HTML Academy</h4><p>ул. Большая Конюшенная, д. 19/8 <br> Санкт-Петербург<br> Россия<br> 197022</p><p><a href="https://htmlacademy.ru/contacts">Свяжитесь с нами</a></p></div>'
     });
 
     marker.addListener("click", function() {
