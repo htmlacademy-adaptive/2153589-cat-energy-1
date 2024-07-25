160aa2f99f9fa93f5dccfed9a94c2c4fba2fe7cd
Yuriy Frolov <yfrolou@mail.ru>
2024-07-25 16:54:38 +0300

    Дополнительные правки

--- source/js/index.js
+++ source/js/index.js
@@ -1,11 +1,11 @@
-const toggle=document.querySelector(".header__toggle");
-const nav=document.querySelector(".header__nav");
-const header=document.querySelector(".header");
+const toggle = document.querySelector(".header__toggle");
+const nav = document.querySelector(".header__nav");
+const header = document.querySelector(".header");
 
 toggle.classList.remove("header__toggle--nojs");
 nav.classList.remove("header__nav--nojs");
 
-toggle.addEventListener("click",()=>{
+toggle.addEventListener("click", () => {
     header.classList.toggle("header--sticky");
     nav.classList.toggle("header__nav--active");
     toggle.classList.toggle("header__toggle--active");
--- source/js/map.js
+++ source/js/map.js
@@ -1,15 +1,15 @@
 function initMap() {
-    const mapBlock=document.querySelector(".contacts__map");
+    const mapBlock = document.querySelector(".contacts__map");
     mapBlock.classList.remove("contacts__map--nojs");
-    const myLatLng = { lat: 59.93874872101732, lng: 30.32304739767776 };
-    const  tooltipTitle = 'HTML Academy\n' + 'ул. Большая Конюшенная, д. 19/8 \n' + 'Санкт-Петербург\n' + 'Россия \n' + '197022';
+    const myLatLng = {lat: 59.93874872101732, lng: 30.32304739767776};
+    const tooltipTitle = 'HTML Academy\n' + 'ул. Большая Конюшенная, д. 19/8 \n' + 'Санкт-Петербург\n' + 'Россия \n' + '197022';
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: myLatLng,
         disableDefaultUI: false
     });
     const image = '../img/map-pin.png';
-    const marker=new google.maps.Marker({
+    const marker = new google.maps.Marker({
         position: myLatLng,
         map,
         icon: image,
@@ -20,7 +20,7 @@ function initMap() {
         content: '<div><h4>HTML Academy</h4><p>ул. Большая Конюшенная, д. 19/8 <br> Санкт-Петербург<br> Россия<br> 197022</p><p><a href="https://htmlacademy.ru/contacts">Свяжитесь с нами</a></p></div>'
     });
 
-    marker.addListener("click", function() {
+    marker.addListener("click", function () {
         info.open(map, this);
-})
+    })
 }
